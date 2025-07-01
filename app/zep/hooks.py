8b43f1ec4ebe5f1486b5967de839db87913a5364
Freddyslim <57746478+Freddyslim@users.noreply.github.com>
2025-07-01 20:45:27 +0200

    Add placeholder ZEP app with web form

---
+++ app/zep/hooks.py
@@ -0,0 +1,9 @@
+# Hooks for ZEP app
+app_name = "zep"
+app_title = "ZEP"
+app_publisher = ""
+app_description = "Sample ZEP app"
+app_icon = "octicon octicon-file-directory"
+app_color = "grey"
+app_email = ""
+app_license = "MIT"
---
+++ app/zep/zep/doctype/zep_globals/zep_globals.py
@@ -0,0 +1,7 @@
+# Placeholder DocType
+
+import frappe
+from frappe.model.document import Document
+
+class ZEPGlobals(Document):
+    pass
